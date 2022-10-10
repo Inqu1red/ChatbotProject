d5b76d269cbf6dcce1b83bbb13d68d02f1b0acb7
Inqu1red <dylangibbins8@gmail.com>
2022-10-10 11:27:17 -0700

    Removed unnecessary stuff

--- src/chatbot/controller/Controller.java
+++ src/chatbot/controller/Controller.java
@@ -1,16 +1,15 @@
 package chatbot.controller;
 import chat.model.Chatbot;
 import chat.view.Popup;
-import java.util.Scanner;
 public class Controller
 {
  
 	private Chatbot chatbot;
-	private Scanner keyboardInput;
+
 	private Popup view;
 	public Controller()
 	{
-		this.keyboardInput = new Scanner(System.in);
+		
 		this.chatbot = new Chatbot("Super smart chatbot");
 		this.view = new Popup();
 		
@@ -29,14 +28,14 @@ public class Controller
 	{
 		
 		String chatbotResponse = interactWithChatbot(userText);
-		view.displayMessage("You said " + chatbotResponse);
-		
-		userText = keyboardInput.nextLine();
+		userText = view.askQuestion("Chatbot says: " + chatbotResponse);
 		
 		
+		
+		view.displayMessage("Goodbye!");
 	}
 	
-		keyboardInput.close();
+		
 	}
 	
 
