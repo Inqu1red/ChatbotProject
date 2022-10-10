1e599cca66fcab7795c28ebed1442290074a7c3c
Inqu1red <dylangibbins8@gmail.com>
2022-10-10 10:36:00 -0700

    Set up popup class to actually do stuff

--- src/chat/vew/Popup.java
+++ src/chat/vew/Popup.java
@@ -1,6 +1,39 @@
 package chat.vew;
-
+import javax.swing.JOptionPane;
+	
 public class Popup
 {
-
+	
+	public void displayMessage(String message)
+	{
+		
+		JOptionPane.showMessageDialog(null, message);
+		
+		
+	}
+	
+	public String askQuestion(String question)
+	{
+		String response = "";
+		
+		response = JOptionPane.showInputDialog(null, question);
+		
+		
+		if (response == null)
+		{
+			
+			response = "";
+			
+		}
+		
+		
+		
+		
+		
+		return response;
+		
+		
+	}
+	
+	
 }
