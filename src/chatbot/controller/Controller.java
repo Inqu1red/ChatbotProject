4a8e3538a4b111bb7d210e115e371e0fe578c9ee
Inqu1red <dylangibbins8@gmail.com>
2022-10-10 12:25:52 -0700

    Makes it say if it's spooky or not

--- src/chatbot/controller/Controller.java
+++ src/chatbot/controller/Controller.java
@@ -6,14 +6,14 @@ public class Controller
 {
  
 	private Chatbot chatbot;
-	public ArrayList<String> spookyThings;
+	
 	private Popup view;
 	public Controller()
 	{
 		
 		this.chatbot = new Chatbot("Super smart chatbot");
 		this.view = new Popup();
-		this.spookyThings = new ArrayList<String>();
+		
 		
 	}
 
@@ -22,15 +22,7 @@ public class Controller
 	public void start()
 	{
 	
-	spookyThings.add("Spiders");
-	spookyThings.add("Scarecrows");
-	spookyThings.add("Blood");
-	spookyThings.add("Horror Movies");
-	spookyThings.add("Ghosts");
-	spookyThings.add("Mummies");
-	spookyThings.add("Werewolves");
-	spookyThings.add("Zombies");
-	spookyThings.add("Trypophobia");
+	
 	String userText = view.askQuestion("What would you like to say to chatbot? ");
 	
 
