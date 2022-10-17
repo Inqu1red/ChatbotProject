aff90232bf8f48c4a95964db73b7c66b7df31e9a
Inqu1red <dylangibbins8@gmail.com>
2022-10-17 11:45:37 -0600

    Update Chatbot.java

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -1,61 +1,58 @@
 package chat.model;
+
 import java.util.ArrayList;
+
 public class Chatbot
 {
-	
-	
+
 	private String username;
 	private String chatbotName;
-	
+
 	public Chatbot(String chatbotName)
 	{
-		
+
 		this.chatbotName = chatbotName;
 		this.username = "";
-		
+
 	}
+
 	public String processText(String input)
 
 	{
-		
+
 		String output = reversePronounDirection(input);
-		
+
 		if (spookyChecker(input))
 		{
-			output += "\n" + spookyResponse() + "\n"; 
-			
-			
+			output += "\n" + spookyResponse() + "\n";
+
 		}
-		if(isValidHTMLChecker(input))
+		if (isValidHTMLChecker(input))
 		{
-			output += "\n" + isValidHTMLResponse() + "\n" ;
+			output += "\n" + isValidHTMLResponse() + "\n";
 		}
 		else
 		{
 			output += "\n" + "No HTML here " + "\n";
-			
+
 		}
 		if (tellGroanJoke(input))
 		{
-			
+
 			output += "\n" + groanJokeResponse() + "\n";
 		}
-		
-		
+
 		return output;
-	
+
 	}
-	
-	
-	
-	
+
 	private Boolean spookyChecker(String sentence)
 	{
-		
+
 		Boolean isSpooky = false;
-		
+
 		ArrayList<String> spookyThings = new ArrayList<String>();
-		
+
 		spookyThings.add("Spiders");
 		spookyThings.add("Scarecrows");
 		spookyThings.add("Blood");
@@ -66,99 +63,92 @@ public class Chatbot
 		spookyThings.add("Zombies");
 		spookyThings.add("Trypophobia");
 		spookyThings.add("Halloween");
-		
-		
-		for(String phrase : spookyThings)
+
+		for (String phrase : spookyThings)
 		{
-			if (sentence.toUpperCase().indexOf( phrase.toUpperCase()) >= 0)
+			if (sentence.toUpperCase().indexOf(phrase.toUpperCase()) >= 0)
 			{
 				isSpooky = true;
-				
-				
+
 			}
-			
-			
-			
+
 		}
-		
+
 		return isSpooky;
 	}
-	 
+
 	private String spookyResponse()
-	 {
-		 String Response = "";
-		 ArrayList<String> spookyResponses = new ArrayList<String>();
-		 spookyResponses.add("woah thats spooky!");
-		 spookyResponses.add("scary!!!");
-		 spookyResponses.add("I love that! Spooky");
-		 spookyResponses.add("Creepy dude");
-		 spookyResponses.add("Bro that's super creepy");
-		 
-		 int randomSpot = (int) (Math.random() * spookyResponses.size()  );
-		 
-		 Response = spookyResponses.get(randomSpot);
-		 
-		 return Response;
-		 
-	 }
-	
+	{
+		String Response = "";
+		ArrayList<String> spookyResponses = new ArrayList<String>();
+		spookyResponses.add("woah thats spooky!");
+		spookyResponses.add("scary!!!");
+		spookyResponses.add("I love that! Spooky");
+		spookyResponses.add("Creepy dude");
+		spookyResponses.add("Bro that's super creepy");
+
+		int randomSpot = (int) (Math.random() * spookyResponses.size());
+
+		Response = spookyResponses.get(randomSpot);
+
+		return Response;
+
+	}
+
 	private Boolean isValidHTMLChecker(String valid)
-	 {
-		 Boolean isValid = true;
-		 if (!valid.contains("<" ))
-		 {
-			 isValid = false;
-			 
-			 return isValid;
-			 
-		 }
-		 
-		 
-		 return isValid;
-	 }
-	 
+	{
+		Boolean isValid = true;
+		if (!valid.contains("<"))
+		{
+			isValid = false;
+
+			return isValid;
+
+		}
+
+		return isValid;
+	}
+
 	private String isValidHTMLResponse()
-	 {
-		
-		 String HTMLResponse = "HTML Is Valid";
-	
-		
-		 return HTMLResponse;
-	 }
-	 
+	{
+
+		String HTMLResponse = "HTML Is Valid";
+
+		return HTMLResponse;
+	}
+
 	private String reversePronounDirection(String input)
-	 {
-		 String output = input.replace("I " , "You ");
-		 output = output.replace(" am "  , " are ");
-		 output = output.replace(" you " , " me ");
-		 
-		 return output;
-	 }
-	 
+	{
+		String output = input.replace("I ", "You ");
+		output = output.replace(" am ", " are ");
+		output = output.replace(" you ", " me ");
+
+		return output;
+	}
+
 	private Boolean tellGroanJoke(String request)
 	{
 		Boolean wantsJoke = false;
-				
+
 		ArrayList<String> jokeAsk = new ArrayList<String>();
 		jokeAsk.add("Tell me a joke");
 		jokeAsk.add("Can you tell me a joke?");
 		jokeAsk.add("Please tell me a joke");
-		
-		for(String joked : jokeAsk)
+
+		for (String joked : jokeAsk)
 		{
-			
-			if(request.toUpperCase().indexOf( joked.toUpperCase()) >= 0)
+
+			if (request.toUpperCase().indexOf(joked.toUpperCase()) >= 0)
 			{
 				wantsJoke = true;
-				
+
 			}
-			
+
 		}
-		
-		
+
 		return wantsJoke;
 	}
-	
+
 	private String groanJokeResponse()
 	{
 		String joke = "";
@@ -171,36 +161,48 @@ public class Chatbot
 		jokes.add("Kid: I'll call you later!" + "\n" + "later: just call me dad!");
 		jokes.add("When does a joke become a dad joke?" + "\n" + "When the punchline become apparent!");
 		jokes.add("I'm worried for the calendar..." + "\n" + "Its days are numbered");
-		int randomSpot = (int) (Math.random() * jokes.size() );
-		
+		int randomSpot = (int) (Math.random() * jokes.size());
+
 		joke = jokes.get(randomSpot);
-		
+
 		return joke;
 	}
-	
+
 	private Boolean tellFunFact(String facts)
 	{
-		
-	Boolean wantsFact = false;
-			
-	ArrayList<String> factAsk = new ArrayList<>();
-	factAsk.add("Tell me a fun fact");
-	factAsk.add("Can you tell me a fact?");
-	factAsk.add("Please tell me a fact");		
-	return wantsFact;
-		
+
+		Boolean wantsFact = false;
+
+		ArrayList<String> factAsk = new ArrayList<>();
+		factAsk.add("Tell me a fun fact");
+		factAsk.add("Can you tell me a fact?");
+		factAsk.add("Please tell me a fact");
+		return wantsFact;
+
 	}
-	
+
+	private String funFacts(String fact)
+	{
+		String tellFact = "";
+		ArrayList<String> facts = new ArrayList<>();
+		facts.add("Did you know that dolphins are actually really mean?");
+		facts.add("Did you know that a group of crows is called a murder?");
+		facts.add("Did you know that ");
+		facts.add("");
+		facts.add("");
+		facts.add("");
+		facts.add("");
+		facts.add("");
+		facts.add("");
+		facts.add("");
+		return tellFact;
+	}
+
 	private boolean computerScienceChecker(String computerFacts)
 	{
 		Boolean wantsComputerFact = false;
-		
-		
+
 		return wantsComputerFact;
 	}
-	
-	
-	
-	
-	
+
 }
