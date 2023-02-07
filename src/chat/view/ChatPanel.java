b23bea05986459bd7803895fed1178adaccbcfd9
Inqu1red <dylangibbins8@gmail.com>
2023-02-07 13:29:49 -0700

    added chatPanel and chatFrame

---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,60 @@
+package chat.view;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.ImageIcon;
+import javax.swing.DefaultComboBoxModel;
+
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Color;
+import java.awt.Dimension;
+import chat.controller.Controller;
+
+
+public class ChatPanel extends JPanel
+{
+
+	private Controller app;
+	
+	
+	public ChatPanel(Controller app)
+	{
+		super();
+		
+		
+		
+		
+	}
+	
+	
+	public void setupPanel()
+	{
+		this.setBackground(Color.BLUE);
+		
+	}
+	
+	public void setupListeners()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	public void setupLayout()
+	{
+		
+	}
+	
+	
+	
+}
