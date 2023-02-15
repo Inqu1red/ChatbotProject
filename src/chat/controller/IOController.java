7b270a27dc93d1245d6adad0f59754c1fbbff470
Inqu1red <dylangibbins8@gmail.com>
2023-02-15 12:02:57 -0700

    saveToTextFile method now collects data and saves it to fileName

--- src/chat/controller/IOController.java
+++ src/chat/controller/IOController.java
@@ -26,6 +26,29 @@ public class IOController
 	public static void saveTextToFile(Controller app, String path, String textToSave)
 	{
 		
+		String fileName = path;
+		
+		LocalDateTime current = LocalDateTime.now(); //gets the local time at the time its used, factory method that gets information for me that is more secure
+		
+		fileName += current.getDayOfMonth() + " " + current.getMonth();
+		
+		int minuteTime = current.getMinute();
+		
+		String minutes = "0";
+		
+		if (minuteTime < 10)
+		{
+			minutes += minuteTime;
+		}
+		else 
+		{
+			minutes = " " + minuteTime;
+		}
+		
+		
+		fileName += " " + current.getHour() + "-" + minutes ;
+		fileName += ".txt";
+		
 	}
 	
 	
